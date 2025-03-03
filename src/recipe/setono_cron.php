a14089990f9a7ba12d166bd32c0f1f191a5c481a
Joachim Løvgaard <joachim@loevgaard.dk>
2025-03-03 15:43:31 +0100

    Fix some text

--- src/recipe/setono_cron.php
+++ src/recipe/setono_cron.php
@@ -9,9 +9,9 @@ require_once 'task/setono_cron.php';
 
 before('deploy:prepare', 'cron:prepare');
 
-// apply the cron just before symlinking. This is where the release_path is available
+// Apply the cron just before symlinking. This is where the release_path is available
 before('deploy:symlink', 'cron:apply');
 
-// cleanup created files
+// Cleanup created files
 after('deploy:cleanup', 'cron:cleanup');
 after('deploy:failed', 'cron:cleanup');
