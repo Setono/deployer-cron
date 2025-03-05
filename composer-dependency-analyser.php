9dfdb9c2f068eea52c62c1a53ffab43450f0900a
Joachim Løvgaard <joachim@loevgaard.dk>
2025-03-05 14:20:31 +0100

    Fix deps

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -4,19 +4,6 @@ use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
-    ->addPathToExclude(__DIR__ . '/tests')
-    ->ignoreUnknownClasses([
-        Deployer\Deployer::class,
-        Deployer\Task\Context::class,
-    ])
-    ->ignoreUnknownFunctions([
-        'Deployer\after',
-        'Deployer\before',
-        'Deployer\get',
-        'Deployer\run',
-        'Deployer\set',
-        'Deployer\task',
-        'Deployer\upload',
-    ])
-    ->ignoreErrorsOnPackage('deployer/deployer', [ErrorType::UNUSED_DEPENDENCY])
+    ->addPathToScan(__DIR__ . '/src', false)
+    ->addPathToExclude(__DIR__ . '/vendor')
 ;
